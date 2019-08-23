9aa8ae2605f592de7e5d89fe293db4e7d83f4946
Clark Alms <clark.alms@gmail.com>
2019-08-23 10:27:19 -0500

    merging to master and pushing to heroku

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Deluxe Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Baconator');
